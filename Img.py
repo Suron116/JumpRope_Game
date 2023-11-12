365bdb8fa17a9e59742350548e6dc6fa1affcd9c
Minjae <flower14288@gmail.com>
2023-11-12 17:01:55 +0900

    불필요한 함수 삭제

--- Img.py
+++ Img.py
@@ -46,19 +46,18 @@ def two_roper(w, h):
 def draw_rope(w, h):
     b_rope = load_image('back_rope.png')
     f_rope = load_image('front_rope.png')
-
+    two_roper(w, h)
     frame = 0
 
     # 700x150
     while True:
         clear_canvas()
+
         level_background.level1_back(w, h)
         two_roper(w, h)
 
-        f = 140
-
-        b_rope.clip_draw(frame * f, 0, 93, 150, w // 2, 2 * h // 3, 2 * w // 3, h // 3)
-        f_rope.clip_draw(frame * f, 0, 93, 150, w // 2, 1 * h // 3, 2 * w // 3, h // 3)
+        b_rope.clip_draw(frame * 140, 0, 93, 150, w // 2, 2 * h // 3, 2 * w // 3, h // 3)
+        f_rope.clip_draw(frame * 140, 0, 93, 150, w // 2, 1 * h // 3, 2 * w // 3, h // 3)
 
         update_canvas()
 
