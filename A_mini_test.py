5f0f28db53867fe9675560bc627d9aca1f195f9c
Minjae <flower14288@gmail.com>
2023-11-12 17:50:30 +0900

    테스트용 플레이어 캐릭터 시작

--- A_mini_test.py
+++ A_mini_test.py
@@ -4,4 +4,10 @@ w = 564
 h = 1001
 open_canvas(w, h)
 
+play_z = load_image('z_character.png')
 
+# 125x150
+while True:
+    clear_canvas()
+    play_z.draw_now(w // 2, h // 2)
+    update_canvas()
