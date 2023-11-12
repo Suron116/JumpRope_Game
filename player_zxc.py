669b783c7661d2a9f27f2af858bfbf3bb6a0907f
Minjae <flower14288@gmail.com>
2023-11-12 18:42:31 +0900

    코드 정리

--- player_zxc.py
+++ player_zxc.py
@@ -1,17 +1,14 @@
 from pico2d import load_image, clear_canvas, delay
 
 class palyers:
-    global jump_hight
-    # 점프 높이 변경
-    jump_hight = 30
-
     @staticmethod
     def player_z(w, h, jump):
         play_z = load_image('z_character.png')
 
+        # 점프 높이 변경
+        jump_hight = 30
         # 125x150
         while True:
-
             if not jump:
                 play_z.draw_now(w // 2, h // 2)
             else:
