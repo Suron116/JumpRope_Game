80ca4f352f7598367fa1ec4876558f666c7c792c
Minjae <flower14288@gmail.com>
2023-12-04 13:27:36 +0900

    배경음 추가

--- Jump_rope_game.py
+++ Jump_rope_game.py
@@ -1,10 +1,14 @@
-from pico2d import open_canvas
+from pico2d import open_canvas, load_music
 import game_type
 
 # 캔버스 사이즈
 w = 564
 h = 1001
 open_canvas(w, h)
+
+bgm = load_music('all_bgm.mp3')
+bgm.set_volume(30)
+bgm.repeat_play()
 
 # 시작화면
 #game_type.start(w, h)
